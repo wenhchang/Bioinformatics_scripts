4c96815564ea82a07d9d141a787fee7787c15eab
wenhchang <wenhsuanc@icloud.com>
2025-06-12 23:27:54 +0800

    2nd update on 250612
    upload more codes, rename some files

---
+++ Code/2.5_visualization_GSEA_Hallmark.R
@@ -0,0 +1,58 @@
+rm(list = ls()) #clear the environment
+
+#load pakcages
+library(tidyverse)
+
+# load data
+GSEA_H <- read.csv("GSEA_H.csv")
+
+## clean up data 
+GSEA_H_data <- GSEA_H_data %>%
+  select(pathway, padj, NES, source) %>%
+  mutate(source = gsub("Pa14C_new", "Pa14C", source), 
+         source = gsub("PANC1", "PANC-1", source), 
+         pathway = gsub("HALLMARK_", "", pathway))
+
+# set the order 
+## manually set the order of hallmark pathway in the figure
+## for the order, I rank the gene sets from the highest medium NES value to the lowest
+
+GSEA_H_data_sumpNES <- GSEA_H_data %>%
+  select(pathway, NES, source) %>%
+  pivot_wider(names_from = source, values_from = NES) %>%
+  # mutate(mean = rowMed(select(., SW1990:Pa14C))) %>%
+  mutate(median = apply(select(., SW1990:Pa14C), 1, median)) %>%
+  arrange(desc(median)) %>%
+  mutate(rank_NES = row_number())
+
+order <- factor(GSEA_H_data_sumpNES$pathway) 
+
+# visualization of GSEA results for Hallmark gene sets
+p1 <- GSEA_H_data %>% #your data set
+  ggplot(aes(x = pathway, y = source, fill = NES, size = -log10(padj))) + #I combined all four cells in the data sets by adding a "source" column
+  geom_point(shape = 21) +
+  theme_classic() +
+  scale_fill_gradient2(low = "blue3", mid = "white", high = "red2", midpoint = 0, 
+                       name = "NES", limits = c(-2, 2), oob = scales::squish)+ #this is to manually set the color representation
+  scale_x_discrete(limits = order)+  # Set x-axis order based on 'order' vector
+  scale_size_continuous(range = c(0.1, 6)) + #the dot size scale
+  # labs(y = "Cell Line",
+  #      x = "Gene set",
+  #      title = "Hallmark gene sets GSEA") +
+  theme(axis.text.y = element_text(size = 12, color = "black"), 
+        axis.text.x = element_text(size = 8, angle = 90, hjust = 1, vjust = 0.5, color = "black"), 
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        # plot.title = element_text(size = 22), 
+        legend.text = element_text(size = 8), 
+        legend.title = element_text(size = 8),
+        legend.position = "bottom",
+        plot.background = element_rect(fill = "transparent", color = NA), # Transparent plot background
+        panel.background = element_rect(fill = "transparent", color = NA), # Transparent panel background
+        legend.background = element_rect(fill = "transparent", color = NA) # Optional: Transparent legend background
+  )
+
+#saving the plot
+png("GSEA.png", width = 5500, height = 2800, res = 600, bg = "transparent")
+print(p1)
+dev.off()
---
+++ Code/2_GSEA_ORA.R
@@ -0,0 +1,159 @@
+# Run GSEA and Over-representation analysis (ORA)
+
+rm(list = ls())                             
+setwd("directory")
+
+# load necessary packages
+library(tidyverse)
+library(msigdbr)
+library(fgsea)
+
+# load gene info table (ensembl_gene_id, gene_symbol)
+gene_info <- read.csv("ref/gene_info.csv")
+
+
+################
+# GSEA
+################
+
+# generate empty df for later storage
+gsea_H  <- data.frame()
+gsea_C2 <- data.frame()
+
+# get gene sets from Molecular Signature Database
+
+## get Hallmark gene sets
+H.ensembl.ls <- msigdbr(species = "Homo sapiens", category = "H") %>% 
+  select(gs_name, ensembl_gene) %>% 
+  group_by(gs_name) %>% 
+  summarise(all.genes = list(ensembl_gene), .groups = "drop") %>% 
+  deframe()
+
+## get C2 gene sets
+C2.ensembl.ls <- msigdbr(species = "Homo sapiens", category = "C2") %>% 
+  select(gs_name, ensembl_gene) %>% 
+  group_by(gs_name) %>% 
+  summarise(all.genes = list(ensembl_gene), .groups = "drop") %>% 
+  deframe()
+
+# Run GSEA
+
+sample_name <- c("PANC1", "Pa14C_new", "Pa16C", "SW1990")
+
+for (cell in sample_name) {
+  
+  raw <- read.csv(
+    paste0("1_DESEQ2_LFCS_240611/", cell, "_low_count_filtered_lfc_DMSO_mtvwt.csv")
+  ) %>% 
+    rename(ensembl_gene_id = X)
+  
+  FC.vec <- raw %>% 
+    select(ensembl_gene_id, log2FoldChange) %>% 
+    { set_names(.$log2FoldChange, .$ensembl_gene_id) } %>% 
+    sort(decreasing = TRUE)
+  
+  temp_H <- fgseaSimple(
+    pathways  = H.ensembl.ls,
+    stat      = FC.vec,
+    scoreType = "std",
+    nperm     = 2000
+  ) %>% 
+    as.data.frame() %>% 
+    mutate(source = cell)
+  
+  temp_C2 <- fgseaSimple(
+    pathways  = C2.ensembl.ls,
+    stat      = FC.vec,
+    scoreType = "std",
+    nperm     = 10000
+  ) %>% 
+    as.data.frame() %>% 
+    mutate(source = cell)
+  
+  gsea_H  <- rbind(gsea_H,  temp_H)
+  gsea_C2 <- rbind(gsea_C2, temp_C2)
+}
+
+######################################
+# Over-representation analysis (ORA)
+######################################
+
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(KEGGgraph)
+library(ReactomePA)
+
+# load gene_info table containing entrez id (not the same gene_info table as above)
+gene_info <- read.csv("ref/gene_info_entrez.csv") %>% mutate(X = NULL)
+
+sample_name <- c("PANC1", "Pa14C_new", "Pa16C", "SW1990")
+
+for (cell in sample_name) {
+  
+  raw <- read.csv(
+    paste0("1_DESEQ2_LFCS_240611/", cell, "_low_count_filtered_lfc_DMSO_mtvwt.csv")
+  ) %>% 
+    rename(ensembl_gene_id = X)
+  
+  FC <- raw %>% 
+    select(ensembl_gene_id, log2FoldChange)
+  
+  # threshold is set as genes upregulated for more than 50%
+  threshold <- log2(1.5)
+  
+  topTen <- FC %>% 
+    filter(log2FoldChange > threshold) %>% 
+    left_join(gene_info, by = "ensembl_gene_id")
+  
+  topTen_list        <- topTen$ensembl_gene_id
+  topTen_list.entrez <- topTen$entrezgene_id
+  
+  # ORA for GO gene sets
+  enrich_go <- enrichGO(
+    gene          = topTen_list,
+    OrgDb         = org.Hs.eg.db,
+    keyType       = "ENSEMBL",
+    ont           = "ALL",
+    pAdjustMethod = "BH",
+    pvalueCutoff  = 1,
+    qvalueCutoff  = 1
+  ) %>% 
+    as.data.frame() %>% 
+    rename(category = ONTOLOGY) %>% 
+    mutate(db = "GO") %>% 
+    select(db, category, Description, GeneRatio, BgRatio, pvalue, p.adjust, qvalue, Count)
+  
+  # ORA for KEGG gene sets
+  enrich_KEGG <- enrichKEGG(
+    gene          = topTen_list.entrez,
+    organism      = "hsa",
+    keyType       = "kegg",
+    pAdjustMethod = "BH",
+    pvalueCutoff  = 1,
+    qvalueCutoff  = 1
+  ) %>% 
+    as.data.frame() %>% 
+    mutate(db = "KEGG") %>% 
+    select(db, category, Description, GeneRatio, BgRatio, pvalue, p.adjust, qvalue, Count)
+  
+  # ORA for Reactome gene sets
+  enrich_Reactome <- enrichPathway(
+    gene          = topTen_list.entrez,
+    organism      = "human",
+    pAdjustMethod = "BH",
+    pvalueCutoff  = 1,
+    qvalueCutoff  = 1
+  ) %>% 
+    as.data.frame() %>% 
+    mutate(db = "REACTOME", category = "") %>% 
+    select(db, category, Description, GeneRatio, BgRatio, pvalue, p.adjust, qvalue, Count)
+  
+  # combine all results
+  Total_ORA <- rbind(enrich_go, enrich_KEGG, enrich_Reactome) %>%
+    mutate(sample = cell)
+  
+  write.csv(
+    Total_ORA,
+    paste0("2_GSEA_240611/ORA_", cell, "_FC1.5_240621.csv")
+  )
+}
